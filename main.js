1603e1137f080607c6b0bf732d89fa8da14cef60
2823027009 <60863804+2823027009@users.noreply.github.com>
2022-04-03 04:51:12 +0800

    up

---
+++ main.js
@@ -0,0 +1,53 @@
+const http = require('http')
+const https = require('https')
+const axios = require('axios')
+const url = require('url')
+const proxlist = require('./proxlist')
+console.log('System start.')
+function _response(res,code,data){
+    res.writeHead(code, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.write(data);
+    res.end();
+};
+http.createServer( function (request, response) {
+    let urlDet = url.parse(request.url);
+    let reqUrl = urlDet.pathname.toString().slice(1);
+    let query = urlDet.query;
+    if(proxlist.index.indexOf(reqUrl) !== -1){
+        let reqObj = proxlist.list[proxlist.index.indexOf(reqUrl)];
+        if(query == "" || query == null){
+            let proxUrl = reqObj.url;
+            axios
+                .get(proxUrl)
+                .then(res => {
+                    _response(response,200,res.data);
+                })
+                .catch(err => {
+                    console.log("Error when get response: " + err);
+                    _response(response,502,'Sever error');
+                });
+        }else if(reqObj.qIndex.indexOf(query) !== -1){
+            let proxUrl = reqObj.query[reqObj.qIndex.indexOf(query)].url;
+            https.get(proxUrl, (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    _response(response,200,data);
+                });
+            }).on("error", (err) => {
+                console.log("Error when get response: " + err.message);
+                _response(response,502,'Sever error');
+            });
+            }else{
+                _response(response,404,'404 not found');
+            }
+        }else{
+        _response(response,404,'404 not found');
+    }
+    }
+).listen(8080);
+
+// 控制台会输出以下信息
+console.log('Server running at http://127.0.0.1:8080/');
---
+++ proxlist.js
@@ -0,0 +1,15 @@
+module.exports ={
+    index: ["openit"],
+    list: [
+        {
+            name: 'openit',
+            url: 'https://cdn.jsdelivr.net/gh/git-yusteven/openit@main/Clash.yaml',
+            qIndex: ['clash','long','ios'],
+            query: [
+                {name: 'clash',url: 'https://cdn.jsdelivr.net/gh/git-yusteven/openit@main/Clash.yaml'},
+                {name: 'long',url: 'https://cdn.jsdelivr.net/gh/git-yusteven/openit@main/long'},
+                {name: 'ios',url: 'https://cdn.jsdelivr.net/gh/git-yusteven/openit@main/https'}
+            ]
+        }
+    ]
+}
