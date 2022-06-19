c441586ae93fc5a4f594d111f2e697b54f4f4439
2823027009 <2823027009@qq.com>
2022-06-20 00:00:50 +0800

    up

--- api/main.js
+++ api/main.js
@@ -1,3 +1,5 @@
+
+
 const http = require('http')
 const https = require('https')
 const axios = require('axios')
@@ -9,43 +11,43 @@ function _response(res,code,data){
     res.write(data);
     res.end();
 };
-http.createServer( function (request, response) {
-    console.log('New request.');
-    let urlDet = url.parse(request.url);
-    let reqUrl = urlDet.pathname.toString().slice(1);
-    let query = urlDet.query;
-    if(proxlist.index.indexOf(reqUrl) !== -1){
-        let reqObj = proxlist.list[proxlist.index.indexOf(reqUrl)];
-        if(query == "" || query == null){
-            let proxUrl = reqObj.url;
-            axios
-                .get(proxUrl)
-                .then(res => {
-                    _response(response,200,res.data);
-                })
-                .catch(err => {
-                    console.log("Error when get response: " + err);
-                    _response(response,502,'Sever error');
-                });
-        }else if(reqObj.qIndex.indexOf(query) !== -1){
-            let proxUrl = reqObj.query[reqObj.qIndex.indexOf(query)].url;
-            axios
-                .get(proxUrl)
-                .then(res => {
-                    _response(response,200,res.data);
-                })
-                .catch(err => {
-                    console.log("Error when get response: " + err);
-                    _response(response,502,'Sever error');
-                });
+moudle.exports = ( function (request, response) {
+        console.log('New request.');
+        let urlDet = url.parse(request.url);
+        let reqUrl = urlDet.pathname.toString().slice(1);
+        let query = urlDet.query;
+        if(proxlist.index.indexOf(reqUrl) !== -1){
+            let reqObj = proxlist.list[proxlist.index.indexOf(reqUrl)];
+            if(query == "" || query == null){
+                let proxUrl = reqObj.url;
+                axios
+                    .get(proxUrl)
+                    .then(res => {
+                        _response(response,200,res.data);
+                    })
+                    .catch(err => {
+                        console.log("Error when get response: " + err);
+                        _response(response,502,'Sever error');
+                    });
+            }else if(reqObj.qIndex.indexOf(query) !== -1){
+                let proxUrl = reqObj.query[reqObj.qIndex.indexOf(query)].url;
+                axios
+                    .get(proxUrl)
+                    .then(res => {
+                        _response(response,200,res.data);
+                    })
+                    .catch(err => {
+                        console.log("Error when get response: " + err);
+                        _response(response,502,'Sever error');
+                    });
             }else{
                 _response(response,404,'404 not found');
             }
         }else{
-        _response(response,404,'404 not found');
+            _response(response,404,'404 not found');
+        }
     }
-    }
-).listen(80);
+);
 
 // 控制台会输出以下信息
 console.log('Server running at http://127.0.0.1:8080/');
--- main.js
+++ main.js
@@ -10,40 +10,40 @@ function _response(res,code,data){
     res.end();
 };
 http.createServer( function (request, response) {
-    console.log('New request.');
-    let urlDet = url.parse(request.url);
-    let reqUrl = urlDet.pathname.toString().slice(1);
-    let query = urlDet.query;
-    if(proxlist.index.indexOf(reqUrl) !== -1){
-        let reqObj = proxlist.list[proxlist.index.indexOf(reqUrl)];
-        if(query == "" || query == null){
-            let proxUrl = reqObj.url;
-            axios
-                .get(proxUrl)
-                .then(res => {
-                    _response(response,200,res.data);
-                })
-                .catch(err => {
-                    console.log("Error when get response: " + err);
-                    _response(response,502,'Sever error');
-                });
-        }else if(reqObj.qIndex.indexOf(query) !== -1){
-            let proxUrl = reqObj.query[reqObj.qIndex.indexOf(query)].url;
-            axios
-                .get(proxUrl)
-                .then(res => {
-                    _response(response,200,res.data);
-                })
-                .catch(err => {
-                    console.log("Error when get response: " + err);
-                    _response(response,502,'Sever error');
-                });
+        console.log('New request.');
+        let urlDet = url.parse(request.url);
+        let reqUrl = urlDet.pathname.toString().slice(1);
+        let query = urlDet.query;
+        if(proxlist.index.indexOf(reqUrl) !== -1){
+            let reqObj = proxlist.list[proxlist.index.indexOf(reqUrl)];
+            if(query == "" || query == null){
+                let proxUrl = reqObj.url;
+                axios
+                    .get(proxUrl)
+                    .then(res => {
+                        _response(response,200,res.data);
+                    })
+                    .catch(err => {
+                        console.log("Error when get response: " + err);
+                        _response(response,502,'Sever error');
+                    });
+            }else if(reqObj.qIndex.indexOf(query) !== -1){
+                let proxUrl = reqObj.query[reqObj.qIndex.indexOf(query)].url;
+                axios
+                    .get(proxUrl)
+                    .then(res => {
+                        _response(response,200,res.data);
+                    })
+                    .catch(err => {
+                        console.log("Error when get response: " + err);
+                        _response(response,502,'Sever error');
+                    });
             }else{
                 _response(response,404,'404 not found');
             }
         }else{
-        _response(response,404,'404 not found');
-    }
+            _response(response,404,'404 not found');
+        }
     }
 ).listen(8080);
 
