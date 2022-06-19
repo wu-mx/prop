c10487b745b3f8c1bc6b21b637013ed6a72a003b
2823027009 <2823027009@qq.com>
2022-06-19 23:58:34 +0800

    up

---
+++ api/proxlist.js
@@ -0,0 +1,41 @@
+module.exports ={
+    index: ["openit","gf","freefq","null"],
+    list: [
+        {
+            name: "openit",
+            url: 'https://raw.githubusercontent.com/git-yusteven/openit/main/Clash.yaml',
+            qIndex: ['clash','long','ios'],
+            query: [
+                {name: 'clash',url: 'https://raw.githubusercontent.com/git-yusteven/openit/main/Clash.yaml'},
+                {name: 'long',url: 'https://raw.githubusercontent.com/git-yusteven/openit/main/long'},
+                {name: 'ios',url: 'https://raw.githubusercontent.com/git-yusteven/openit/main/https'}
+            ]
+        },
+        {
+            name: "gf",
+            url: 'https://dllx.tk/GreenFishYYDS-Clash',
+            qIndex: ['clash','v2'],
+            query: [
+                {name: 'clash',url: 'https://dllx.tk/GreenFishYYDS-Clash'},
+                {name: 'v2',url: 'https://dllx.tk/GreenFishYYDS'}
+            ]
+        },
+        {
+            name: "freefq",
+            url: 'https://api.v1.mk/sub?target=clash&url=https%3A%2F%2Fraw.githubusercontent.com%2Ffreefq%2Ffree%2Fmaster%2Fv2&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FMeilieage%2Fwebcdn%2Fmain%2Frule%2FArea_Media_NoAuto.ini&emoji=true&list=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&sort=false&new_name=true',
+            qIndex: ['clash','v2'],
+            query: [
+                {name: 'clash',url: 'https://api.v1.mk/sub?target=clash&url=https%3A%2F%2Fraw.githubusercontent.com%2Ffreefq%2Ffree%2Fmaster%2Fv2&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FMeilieage%2Fwebcdn%2Fmain%2Frule%2FArea_Media_NoAuto.ini&emoji=true&list=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&sort=false&new_name=true'},
+                {name: 'v2',url: 'https://raw.githubusercontent.com/freefq/free/master/v2'}
+            ]
+        },
+        {
+            name: "null",
+            url: 'https://hi.nullnode.workers.dev/nullnode-clash',
+            qIndex: ['clash'],
+            query: [
+                {name: 'clash',url: 'https://hi.nullnode.workers.dev/nullnode-clash'}
+            ]
+        }
+    ]
+}
