187437aa878de7904bf12e472e3c1865fe9f0681
2823027009 <2823027009@qq.com>
2022-08-23 11:59:12 +0800

    up

---
+++ api/fdc.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+console.log('System start.')
+function _response(res,code,data){
+    res.status(code)
+    res.send(data);
+    res.end();
+};
+module.exports = ( function (request, response) {
+        let reqUrl = request.query.url;
+        axios.get({
+            method: 'get',
+            url: reqUrl,
+            headers: {
+                'User-Agent': 'ClashforWindows/0.19.21'
+            }
+        }).then(function (res) {
+            _response(response,200,res.data);
+        }
+        ).catch(function (err) {
+            _response(response,500,err);
+        })
+    });
